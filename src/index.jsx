136527ccde61ef4de8bd80373c8716a7141ec56c
jacob ruleaux and Michelle Poterek <michelle@>
2017-09-25 10:42:25 -0700

    set up basic project structure

---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import styles from "./styles/styles.css";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  reducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("react-app-root")
+);
